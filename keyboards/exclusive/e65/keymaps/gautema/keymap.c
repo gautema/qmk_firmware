8da080fa7c9e33df97baebbdbe1ab4138d85cfa7
Gaute Magnussen <gaute.magnussen@gmail.com>
2020-03-18 08:23:15 +0100

    Changes to E65

--- keyboards/exclusive/e65/keymaps/gautema/keymap.c
+++ keyboards/exclusive/e65/keymaps/gautema/keymap.c
@@ -2,7 +2,7 @@
 
 /*
  * ,---------------------------------------------------------------------|
- * |`  |1  |2  |3  |4  |5  |6  |7  |8  |9  |0   |-   |=  | ~ | |\ | HOME |
+ * |`  |1  |2  |3  |4  |5  |6  |7  |8  |9  |0   |-   |=  | |\ | `~ | HOME |
  * |---------------------------------------------------------------------|
  * |Tab  |Q  |W  |E  |R  |T  |Y  |U  |I  |O  |P  |[  | ]  |   BS     |Del|
  * |---------------------------------------------------------------------|
@@ -16,17 +16,17 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_65_ansi_blocker_splitbs(
-        KC_GESC, KC_1,   KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0, KC_MINS, KC_EQL,  KC_BSLS, KC_GRV, KC_DEL,
-        KC_TAB, KC_Q,   KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P, KC_LBRC, KC_RBRC, KC_BSPC, KC_PGUP,
-        KC_RCTRL, KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,        KC_ENT, KC_PGDN,
-        KC_LSFT, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M, KC_COMM, KC_DOT,KC_SLSH,         KC_RSFT, KC_UP, KC_HOME,
+        KC_GESC, KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0, KC_MINS, KC_EQL,  KC_BSLS, KC_GRV, KC_HOME,
+        KC_TAB,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P, KC_LBRC, KC_RBRC, KC_BSPC, KC_DEL,
+        MO(1),   KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,        KC_ENT, KC_PGUP,
+        KC_LSFT, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M, KC_COMM, KC_DOT,KC_SLSH,        KC_RSFT, KC_UP,  KC_PGDN,
         KC_LCTL, KC_LALT, KC_LGUI,                  KC_SPC,                      KC_RALT, MO(1), KC_LEFT, KC_DOWN, KC_RGHT),
 
     [1] = LAYOUT_65_ansi_blocker_splitbs(
         KC_GRV,  KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12, _______, _______, KC_VOLU,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_DEL, KC_VOLD,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP,_______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL, KC_VOLD,
+        _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______, KC_HOME,
+        _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP,KC_END,
         _______, _______, _______,                  _______,                             MO(2),   MO(1), KC_HOME, KC_PGDN, KC_END),
 
     [2] = LAYOUT_65_ansi_blocker_splitbs(
